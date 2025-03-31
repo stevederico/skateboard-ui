1f39a534e239d5f17e4e3cfb1ce4b69f26901de3
Steve Derico <steve@bixbyapps.com>
2025-03-31 15:47:58 -0700

    0.3.4

--- Sheet.jsx
+++ Sheet.jsx
@@ -2,17 +2,17 @@ import { useState, useRef, useImperativeHandle, forwardRef } from 'react';
 import {
   Sheet,
   SheetContent,
-  SheetDescription,
   SheetHeader,
   SheetTitle,
-  SheetClose,
 } from "./shadcn/ui/sheet"
 
 const MySheet = forwardRef(function MySheet(props, ref) {
-  const { title, description, children, showCloseButton } = props;
+  const { title = "", children } = props;
   const [isOpen, setIsOpen] = useState(false);
   
   useImperativeHandle(ref, () => ({
+    show: () => setIsOpen(true),
+    hide: () => setIsOpen(false),
     open: () => setIsOpen(true),
     close: () => setIsOpen(false),
     toggle: () => setIsOpen(prev => !prev)
@@ -20,19 +20,13 @@ const MySheet = forwardRef(function MySheet(props, ref) {
 
   return (
     <Sheet open={isOpen} onOpenChange={setIsOpen}>
-      <SheetContent className="bg-background w-[400px] sm:w-[540px]" side="bottom">
-        {(title || description) && (
-          <SheetHeader>
-            {title && <SheetTitle>{title}</SheetTitle>}
-            {description && <SheetDescription>{description}</SheetDescription>}
-          </SheetHeader>
-        )}
-        {children}
-        {showCloseButton && (
-          <SheetClose className="mt-4 bg-accent px-4 py-2 rounded-md hover:opacity-90">
-            Close
-          </SheetClose>
-        )}
+      <SheetContent className="bg-background w-full" side="bottom">
+        <SheetHeader className={"mb-0"}>
+          {title && <SheetTitle>{title}</SheetTitle>}
+        </SheetHeader>
+        <span className="mx-4 mb-4">{children}</span>
+
+        
       </SheetContent>
     </Sheet>
   );
