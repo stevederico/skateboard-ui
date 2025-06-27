0900682b0c427e193045a5e04e5a3856592250d6
Steve Derico <steve@bixbyapps.com>
2025-06-26 18:29:55 -0700

    0.5.7

--- SettingsView.jsx
+++ SettingsView.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getState } from '@/context.jsx';
 import { useEffect, useState } from 'react';
--- SignUpView.jsx
+++ SignUpView.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { cn } from "./shadcn/lib/utils"
 import { Button } from "./shadcn/ui/button"
 import {
