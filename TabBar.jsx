2b64bfdc5f7c10105a5f8d08aabda5fc37134bf6
Steve Derico <steve@bixbyapps.com>
2025-06-26 18:07:40 -0700

    0.5.5

--- TabBar.jsx
+++ TabBar.jsx
@@ -1,6 +1,15 @@
+import React from 'react';
 import constants from "@/constants.json";
 import { Link, useLocation } from 'react-router-dom';
-import { DynamicIcon } from "./lucide-react/dynamic"; // Verify this import
+import * as LucideIcons from "lucide-react";
+
+// Dynamic Icon Component
+const DynamicIcon = ({ name, size = 24, color = 'currentColor', strokeWidth = 2, ...props }) => {
+  const toPascalCase = (str) => str.split(/[-_\s]/).map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join('');
+  const possibleNames = [name, toPascalCase(name), name.charAt(0).toUpperCase() + name.slice(1)];
+  const LucideIcon = possibleNames.find(n => LucideIcons[n]) ? LucideIcons[possibleNames.find(n => LucideIcons[n])] : null;
+  return LucideIcon ? React.createElement(LucideIcon, { size, color, strokeWidth, ...props }) : null;
+};
 
 export default function TabBar() {
   const location = useLocation();
