38c6df06fea5176e6ea0b407634450ffd9c77711
Steve Derico <steve@bixbyapps.com>
2025-08-07 16:11:42 -0700

    0.7.0

--- shadcn/ui/separator.jsx
+++ shadcn/ui/separator.jsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import * as SeparatorPrimitive from "@radix-ui/react-separator"
 
-import { cn } from "../lib/utils"
+import { cn } from "@/shadcn/lib/utils"
 
 function Separator({
   className,
@@ -13,7 +13,7 @@ function Separator({
 }) {
   return (
     (<SeparatorPrimitive.Root
-      data-slot="separator-root"
+      data-slot="separator"
       decorative={decorative}
       orientation={orientation}
       className={cn(
