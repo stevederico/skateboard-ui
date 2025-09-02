e21e880c1679ff6a1d8312a87430ddf6ef47aa57
Steve Derico <steve@bixbyapps.com>
2025-09-02 10:23:01 -0700

    0.8.1

--- Header.jsx
+++ Header.jsx
@@ -4,7 +4,7 @@ function Header(props) {
         <span className="font-semibold text-2xl">{props.title}</span>
         {typeof props.buttonTitle !== "undefined" && (
           <button
-            className={`ml-auto bg-app text-white px-4 py-2 rounded mr-0 ${props.buttonClass || ''}`}
+            className={`ml-auto bg-app text-white px-4 py-2 rounded mr-0 cursor-pointer ${props.buttonClass || ''}`}
             onClick={props.onButtonTitleClick}
           >
             {props.buttonTitle}
